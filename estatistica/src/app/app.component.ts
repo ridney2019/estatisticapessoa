776e245c90b28eb5a37c9d2c8e4a1d846109c3e4
ridney2019 <ridneygbs@gmail.com>
2020-04-14 22:45:56 -0300

    Estatístistica Pessoas

--- estatistica/src/app/app.component.ts
+++ estatistica/src/app/app.component.ts
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'estatistica';
+  //Declaração do atributos e lista.
+  nome =null;
+  idade=null;
+  pessoas=[];
+  pessoaMaisJovem=null;
+  pessoaMaisVelha=null;
+
+
+//Método salvar, adicionar pessoa na lista
+//atributos null, limpar os campos de input.
+  salvar(){
+    this.pessoas.push({
+      nome: this.nome,
+      idade: this.idade
+    });
+    this.nome = null;
+    this.idade = null;
+    this.atualizarEstatisticas();
+  } 
+
+
+  //Método splice, trata a exclusão a partir do
+  //índice do aray do objeto e segundo parâmetro indica a qtd de elementos excluído é 1.
+  remover(pessoa){
+     const i = this.pessoas.indexOf(pessoa);
+     this.pessoas.splice(i, 1);
+     this.atualizarEstatisticas();
+  }  
+
+
+  //Recurso de ordenação.
+  //Object, cria uma cópia do objeto pessoas passado como parâmetro.
+  //Método sort, recebe função como parâmetro, função seta utilizada. 
+  atualizarEstatisticas(){
+    let copia = Object.create(this.pessoas);
+    copia.sort((p1: any, p2: any ) => p1.idade - p2.idade);
+    this.pessoaMaisJovem = copia[0];
+
+    copia.sort((p1: any, p2: any ) => p2.idade - p1.idade);
+    this.pessoaMaisVelha = copia[0];
+    
+  }
 }
